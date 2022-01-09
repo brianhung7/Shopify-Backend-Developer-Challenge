af1e8ccb5e9f238b3665d282ea57cf9057d822b6
brianhung7 <brian.texts@gmail.com>
2022-01-09 15:10:40 -0800

    Server js setup

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express(); 
+const methodOverride = require("method-override")
+
+//Controllers
+const productCtrl = require("./controllers/productcontroller.js");
+
+//Routes
+app.use("/", productCtrl)
 
 
-app.get('/', function(request,response){
-    response.send("Hello World"); 
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+app.get('/*', function(request,response){
+    response.send("Wrong way! 404ed!"); 
 });
 
 app.listen(4000, function(){
     console.log("I am live on port 4000");
-  });
+});
